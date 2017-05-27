14bcd9f2248466761c7e9beed528b3e46975f3c8
Daniel Crocco <dccdany93@gmail.com>
2017-05-27 14:11:30 -0300

    [ADD] Agregado de rutas nuevas

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-   get 'register_food/index'
+  get '/foods/listed_food' => 'food#listed_food'
+  resources :food
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
