d0daf07ec9420f9fbe3013885fd6c0b697b2a38c
euge <matytaa@gmail.com>
2017-06-10 03:00:52 -0300

    Oculto proyecto en comensales

--- app/models/comensale.rb
+++ app/models/comensale.rb
@@ -3,7 +3,6 @@ class Comensale < ActiveRecord::Base
 	validates :id_legajo, :presence => {:message => "Debe ingresar su número de legajo"}
 	validates :mail, :presence => {:message => "Debe ingresar su email"}
 	validates :tipo_comensal, :presence => {:message => "Debe ingresar el Tipo de Comensal"}
-	validates :proyecto, :presence => {:message => "Debe ingresar el nombre del proyecto por el que esta informando"}
 	validates :id_legajo, :presence => {:message => "Debe ingresar el número de legajo"}
 	validates :cantidad, :presence => {:message => "Debe ingresar la cantidad de Comensales"}
 
